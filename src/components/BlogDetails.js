967640e5b10cf47cf6075c71a65cf5d90b67e953
Arkadiusz Zygan <a.zygan@gmail.com>
2021-01-21 12:08:15 +0100

    Reuse useFetch

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,10 +1,25 @@
 import { useParams } from "react-router-dom";
 
+import useFetch from "../useFetch";
+
 const BlogDetails = () => {
   const { id } = useParams();
+
+  const { data: blog, isPending, error } = useFetch(
+    `http://localhost:8000/blogs/${id}`
+  );
+
   return (
     <div className='blog-details'>
-      <h2>Blog details - {id}</h2>
+      {error && <div>{error}</div>}
+      {isPending && <h2>Loading...</h2>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
