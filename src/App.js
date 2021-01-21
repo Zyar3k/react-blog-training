e54be7d6f8d6973947857c16a99f5d9c35190d63
Arkadiusz Zygan <a.zygan@gmail.com>
2021-01-21 11:35:28 +0100

    Add react-router to project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <div className='content'>
-        <Home />
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route path='/' component={Home} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
